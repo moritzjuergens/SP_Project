e35baccb1dfcffce6714702bf22f095874338632
moritzjuergens <moritzjuergens@outlook.de>
2021-05-08 12:06:29 +0200

    adds database; this time hopefully for real

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,14 +3,20 @@ package controllers
 import play.api.mvc._
 //import play.api.db._
 import javax.inject.{Inject, _}
-
+import daos.ProduktDAO
+import model.Produkt
+import play.api._
+import play.api.data.Form
+import play.api.data.Forms.{mapping, number, text}
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(produktDao: ProduktDAO, controllerComponents: ControllerComponents)
+                              (implicit executionContext: ExecutionContext) extends AbstractController(controllerComponents) {
 
   /**
    * Create an Action to render an HTML page.
@@ -19,8 +25,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index() = Action.async {
+    produktDao.all().map { case (produkte) => Ok(views.html.index(produkte)) }
   }
 
   def hello(name: String) = Action {
@@ -33,5 +39,20 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
   def about() = Action {
     Ok(views.html.about())
+  }
+
+  def env() = Action { implicit request: Request[AnyContent] =>
+    Ok("Nothing to see here")
+    //Ok(System.getenv("JDBC_DATABASE_URL"))
+  }
+
+  val produktForm = Form(
+    mapping(
+      "name" -> text(),
+      "price" -> number())(Produkt.apply)(Produkt.unapply))
+
+  def insertProdukt = Action.async { implicit request =>
+    val produkt: Produkt = produktForm.bindFromRequest.get
+    produktDao.insert(produkt).map(_ => Redirect(routes.HomeController.index))
   }
 }
--- build.sbt
+++ build.sbt
@@ -8,16 +8,17 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.13.5"
 
 herokuAppName in Compile := "afternoon-temple-82615"
-
+herokuConfigVars in Compile := Map(
+  "DATABASE_URL" -> "postgres://idwhdhjxarjdvb:aa9c68fcf0c3da083f9c8afffe8cd83d7951a35585c1f2c9f6f04e975adb6c74@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d5fcu1g83515ur",
+)
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+libraryDependencies += "org.postgresql" % "postgresql" % "42.2.6"
 
-libraryDependencies += "org.postgresql" % "postgresql" % "9.4-1200-jdbc41"
 libraryDependencies ++= Seq(
-  jdbc,
-  "org.playframework.anorm" %% "anorm" % "2.6.10"
+  "com.typesafe.play" %% "play-slick" % "5.0.0",
+  "com.typesafe.play" %% "play-slick-evolutions" % "5.0.0"
 )
-
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.example.controllers._"
 
